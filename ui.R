f7dd529a486b8815e7d7d7d330094df1dbc27a05
jstratton <jamstratton@gmail.com>
2016-03-02 16:55:02 -0500

    "Added User Interface to App"

--- ui.R
+++ ui.R
@@ -1,2 +1,16 @@
 library(shiny)
-shinyUI()
+shinyUI(pageWithSidebar(
+        headerPanel("Temperature Unit Converter"),
+        sidebarPanel(
+                numericInput("temperature", label = "Temperature", value = 0),
+                radioButtons("unit", label = "Degrees", 
+                             choices = c("Fahrenheit", "Celsius"))),
+        mainPanel(
+                h4("Is Equivalent To"),
+                
+                verbatimTextOutput("converted_temp"),
+                
+                radioButtons("conversion", label = "Degrees", 
+                             choices = c("Fahrenheit", "Celsius"))
+        )
+))
