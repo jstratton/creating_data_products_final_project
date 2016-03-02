a6096da327096e0852f5ff8e19e94f4e4a97b931
jstratton <jamstratton@gmail.com>
2016-03-02 17:34:49 -0500

    Finished writing the server.R for the App

--- server.R
+++ server.R
@@ -1,6 +1,24 @@
 library(shiny)
+
 shinyServer(
         function(input, output){
+                conversionFactor <- reactive({
+                        # No conversion is necessary if the units are the same
+                        if(as.character(input$unit) == as.character(input$conversion)){
+                                input$temperature
+                        }
+                        else{
+                                # Fahrenheit to Celsius Conversion
+                                if(as.character(input$conversion) == "Celsius"){
+                                        (input$temperature - 32)*5/9
+                                }
+                                # Celsius to Fahrenheit Conversion
+                                else{
+                                        input$temperature*9/5 + 32
+                                }
+                        }
+                })
                 
+                output$converted_temp = renderText({conversionFactor()})
         }
 )
