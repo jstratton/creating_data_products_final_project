8eb721dbe37cf68cc0b141c704f3a79a2fb4ec84
jstratton <jamstratton@gmail.com>
2016-03-04 11:07:01 -0500

    Added a text explanation to document the app.

--- ui.R
+++ ui.R
@@ -10,6 +10,20 @@ shinyUI(pageWithSidebar(
         mainPanel(
                 h4("Converted Temperature"),
                 
-                verbatimTextOutput("converted_temp")
+                verbatimTextOutput("converted_temp"),
+                
+                h4("This app takes temperatures in degrees Celsius and carries
+                   out the calculations to get the temperature in degrees
+                   Fahrenheit, and vice versa."),
+                
+                h4("Enter the temperature to be converted and select its given units using
+                   the inputs to the left."),
+                
+                h4("The app automatically converts to the unselected temperature
+                        unit when you enter a temperature."),
+                
+                h4("For example, if degrees Fahrenheit is selected as the entered
+                        temperature's unit the app automatically converts to
+                        degrees Celsius.")
         )
 ))
